66fa4382d2f0a34113ca4351c3c91ee4edbaf87d
kimdeokyoung <dudejrss@naver.com>
2023-02-28 16:58:00 +0900

    chore: test react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>header</div>
+      <div>
+        <h1>content</h1>
+        <Routes basename={process.env.PUBLIC_URL}>
+          <Route path="info" element={<div>info</div>}/>
+          <Route path="pages" element={<div>pages</div>}/>
+        </Routes>
+      </div>
+      <div>footer</div>
     </div>
   );
 }
