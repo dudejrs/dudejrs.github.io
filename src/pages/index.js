280c67c14da774bfa4facb9531cfef2e82e35c6b
kimdeokyoung <dudejrss@naver.com>
2023-05-06 22:10:00 +0900

    feat : add Main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 
 import {Routes, Route} from 'react-router-dom';
-import Info from './info'
+import Info from './info';
+import Main from './main';
 
 function Pages() {
   return (
       <Routes basename={process.env.PUBLIC_URL}>
+        <Route path="" element={<Main />}/>
         <Route path="info" element={<Info />}/>
         <Route path="projects" >
           <Route path="cenema" element={<div> 준비중입니다.. </div>}/>
---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+
+import styles from './index.module.css'
+
+export default function Main({}){
+	return (
+		<div className={styles.container}>
+			< img src="https://ghchart.rshah.org/dudejrs" />
+		</div>
+		);
+}
