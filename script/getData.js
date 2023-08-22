2c5894e04d5e35551fbae237f3d7c76f0501afe5
kimdeokyoung <dudejrss@naver.com>
2023-08-22 12:16:14 +0900

    feat : script/getData/fetchCodingPractice

--- script/getData.js
+++ script/getData.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const {getDatabase, getPage} = require('./notion');
 const {getPlans} = require('./notion/plan');
 const {getDetailedPlansFromPlans} = require('./notion/detailedPlan')
-const {getTotalProblems} = require("./notion/codingPractice");
+const {getCodingPracticeAggregation} = require("./notion/codingPractice");
 
 
 require('dotenv').config();
@@ -14,11 +14,14 @@ require('dotenv').config();
 const planDirPath = 'public/data/plan';
 const detailedPlanDirPath = 'public/data/detailedPlan'
 const planMetaDataPath = `${planDirPath}/meta.json`
+const codingPracticeDirPath = 'public/data/codingPractice'
 
 const planFilterList = ['categories.json']
 
 const tags = ["Javascript", "Java", "DBMS", "Backend", "DevOps", "Data Science", "Graphics"];
+
 const langauges = ["Python","Javascript","C++","Java"]
+
 const notion = new Client({
 	auth : process.env.notion_integration_secret
 });
@@ -35,7 +38,7 @@ const fetchPlans= ()=>{
 	}
 
 	getPlans(notion, tags, planDirPath, process.env.notion_integration_secret);
-	writeMetaData();
+	writeMetaData(planDirPath);
 }
 
 
@@ -49,33 +52,41 @@ const fetchDetailedPlans= ()=>{
 
 
 	getDetailedPlansFromPlans(planDirPath, detailedPlanDirPath, planFilterList, process.env.notion_integration_secret);
-	writeMetaData();
+	writeMetaData(detailedPlanDirPath);
 }
 
-
-const writeMetaData = ()=>{
-	const currentDateString = new Date(Date.now()).toISOString();
+const fetchCodingPractice = ()=>{
 	try {
-		origin = JSON.parse(fs.readFileSync(planMetaDataPath));
+		fs.rmdirSync(codingPracticeDirPath, {recursive:true}, (err)=>{console.log(err)})
+	}catch(error){
+	}finally {
+		fs.mkdirSync(codingPracticeDirPath ,(err)=>{console.log(err)});
+	}
+	getCodingPracticeAggregation(notion, langauges, codingPracticeDirPath);
+	writeMetaData(codingPracticeDirPath);
+}
+
+const writeMetaData = (dirPath)=>{
+	const currentDateString = new Date(Date.now()).toISOString();
+	const metaDataPath = `${dirPath}/meta.json`
+	try {
+		origin = JSON.parse(fs.readFileSync(metaDataPath));
 		origin.updated = currentDateString.slice(0,10)
-		fs.writeFileSync(planMetaDataPath, JSON.stringify(origin));
+		fs.writeFileSync(metaDataPath, JSON.stringify(origin));
 	}catch (error) {
 		console.log(error);
 	}
 	finally{
 		origin = {}
 		origin.updated = currentDateString.slice(0,10)
-		fs.writeFileSync(planMetaDataPath, JSON.stringify(origin));
+		fs.writeFileSync(metaDataPath, JSON.stringify(origin));
 	}
 }
 
 
-const test = ()=>{
-	console.log(getTotalProblems(notion, langauges));
-}
 
-test();
 
 // fetchPlans();
 // fetchDetailedPlans();
-// writeMetaData();
+fetchCodingPractice();
+
--- script/notion/codingPractice.js
+++ script/notion/codingPractice.js
@@ -1,6 +1,16 @@
 const {getProperty} = require('./index.js'); 
+const fs = require('fs');
 
+async function getCodingPracticeAggregation(notion, langauges, dirPath){
+	let totalProblem = await getTotalProblems(notion, langauges);
+	let aggregationByCategries = await getAggregationByCategories(notion, langauges);
+	let aggregationByProblemType = await getAggregationByProblemType(notion, langauges);
 
+	fs.writeFile(`${dirPath}/totalProblem.json`, JSON.stringify(totalProblem), (err)=>{console.log(err);});
+	fs.writeFile(`${dirPath}/aggregationByCategries.json`, JSON.stringify(aggregationByCategries), (err)=>{console.log(err);});
+	fs.writeFile(`${dirPath}/aggregationByProblemType.json`, JSON.stringify(aggregationByProblemType), (err)=>{console.log(err);});
+
+}
 
 
 async function getTotalProblems(notion, langauges) {
@@ -25,7 +35,7 @@ async function getTotalProblems(notion, langauges) {
 	})
 
 	let next_cursor;
-
+	
 	while(has_more_){
 		let {has_more, next_cursor, results} = await notion.databases.query(query);
 		query["start_cursor"] = next_cursor;
@@ -33,17 +43,100 @@ async function getTotalProblems(notion, langauges) {
 		count += results.length;
 		repetition += results.reduce((acc, result) => acc+ result["properties"]["Repetition"]["number"], 0);
 	}
-	console.log(count, repetition)
-	return {count, repetition}
+
+	return {count, repetition};
 }
 
-function getTotalAlgorithmProblems(notion){
+async function getAggregationByCategories(notion, langauges){
 
-	return {};
+	let has_more_ = true;
+
+	let query = {
+		database_id : process.env.notion_coding_pracitce_category_database_id,
+	};
+
+	let results_ = [];
+
+	while(has_more_){
+		let {has_more, next_cursor, results} = await notion.databases.query(query);
+		query["start_cursor"] = next_cursor;
+		has_more_ = has_more;
+		results_ = results_.concat(results)
+	}
+
+	results_= results_.map((result)=> refineAggregation(result, langauges));
+
+	return results_;
 }
 
-function getTotalDesignPatternProblems(notion){
-	return {};
+
+function refineAggregation(result, langauges){
+
+
+	const name = result["properties"]["Name"]["title"][0]["plain_text"]
+	const count = result["properties"]["count"]["formula"]["number"]
+	const repetition = result["properties"]["Repetition"]["rollup"]["number"]
+
+	const r = {name, count, repetition}
+
+	langauges.forEach((langauge) => r[langauge] = result["properties"][langauge]["rollup"]["number"]);
+
+	return r;
 }
 
-module.exports = {getTotalProblems}
+
+async function getAggregationByProblemType(notion, langauges){
+
+	let has_more_ = true;
+
+	let query = {
+		database_id : process.env.notion_coding_practice_database_id,
+	}
+
+	let countByTypes = {};
+
+	while(has_more_){
+		let {has_more, next_cursor, results} = await notion.databases.query(query);
+		query["start_cursor"] = next_cursor;
+		has_more_ = has_more;
+
+		results.forEach((result) => aggregateResult(result, countByTypes, langauges));
+	}
+
+	return countByTypes;
+}
+
+function aggregateResult(result, countByTypes, langauges){
+
+	const problemTypes = result["properties"]["문제유형"]["multi_select"].map((m)=> m["name"]);
+
+	const resultCounts = {
+		count : 1,
+		repetition : 0,
+	};
+
+	resultCounts.repetition += result["properties"]["Repetition"]["number"]
+
+
+	langauges.forEach((langauge) => {
+		resultCounts[langauge] = 0 + result["properties"][langauge]["checkbox"];
+	})
+
+	problemTypes.forEach((type)=> {
+		if(!countByTypes[type]) {
+			countByTypes[type] = resultCounts;
+			return;
+		}
+
+		countByTypes[type]["count"] += resultCounts.count;
+		countByTypes[type]["repetition"] += resultCounts.repetition;
+
+		langauges.forEach((langauge) => {
+			countByTypes[type][langauge] += resultCounts[langauge];
+		})
+
+	});
+	
+}
+
+module.exports = {getCodingPracticeAggregation}
