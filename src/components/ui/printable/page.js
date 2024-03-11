f46a5511685a166211dd1d41dff428e57bd54ed4
kimdeokyoung <dudejrss@naver.com>
2024-03-11 19:14:37 +0900

    chore: change styles

---
+++ src/components/ui/printable/page.js
@@ -0,0 +1,11 @@
+import styles from './printable.module.css'
+
+export default function({className, children}) {
+	return (
+			<div className={`${className} ${styles.section}`}>
+				{
+					children
+				}
+			</div>
+		)
+}
