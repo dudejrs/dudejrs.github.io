33abc82348af6a9832ad5665a10e257f069f4614
kimdeokyoung <dudejrss@naver.com>
2025-09-08 11:36:46 +0900

    fix : react-script와 eslint9 버전 충돌로 인한 github action 가동안되는 문제

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+    root: true,
+    ignorePatterns: [
+        'node_modules/**/*',
+    ],
+    env: {
+        browser: true,
+        node: true,
+    },
+    globals: {
+        process: 'readonly',
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+    ],
+    plugins: [
+        'react',
+    ],
+    overrides: [
+        {
+            files: ['src/**/*.{js,jsx,ts,tsx}'],
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: 'module',
+                ecmaFeatures: { jsx: true },
+            },
+            env: {
+                es6: true,
+                browser: true,
+            },
+        },
+        {
+            files: ['script/**/*.js'],
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: 'commonjs',
+            },
+            env: {
+                es6: true,
+                node: true,
+            },
+        },
+    ],
+    rules: {
+        'react/prop-types': 'off',
+        'no-unused-vars': [
+            'error',
+            {
+                ignoreRestSiblings: true,
+                destructuredArrayIgnorePattern: '^_',
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+            },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
